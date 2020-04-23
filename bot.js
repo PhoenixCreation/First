adbf5426d6d83c29d91109970baa742306a39c69
PhoenixCreation <63447220+PhoenixCreation@users.noreply.github.com>
2020-04-23 13:57:33 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,23 @@
 console.log('Bot is starting')
 
+//twit is package used for twitter connection.
 var Twit = require('twit');
 
 
-
+// Enter you api keys here in appropriate fields. You can get your API keys on devloper account of twitter.
 var T = new Twit({
-  consumer_key:         'AM7dUHfhhRzp0etkEb6MlRV4A',
-  consumer_secret:      '1KrTMwyhpkRcwk3IYAbUB1TWn2mVBLeHRDp088ssrDAOYOtBOF',
-  access_token:         '1248464980908724225-QTIHRuAw0uwiwtzaMoXUuDAqujGZaG',
-  access_token_secret:  'LY30zdQTQj06rG7mCE7ALaYE7PByUV06zJcfuZTVBAfVh',
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
 });
-
+//here 'stream' variable will continously check for tweets with '@PhienixCrea2ion'
 var stream = T.stream('statuses/filter', { track: '@PhoenixCrea2ion' })
+//here 's2' varible will continously check for tweets with '#createphoenix'
 var s2=T.stream('statuses/filter', { track: '#createphoenix' })
+//if '@PhoenixCrea2ion' is found in tweet call followed function. This is not for following trigger.
 stream.on('tweet',followed)
+//if '#createphoenix' is found in tweet call hashedme function.
 s2.on('tweet',hashedme)
 
 function hashedme(eventMsg){
@@ -39,7 +43,8 @@ function followed(eventMsg){
     console.log('-------------------------------------------------');
 
 }
-// twitThat('having bad internet is very bad thing')
+
+//function that tweets from your account
 function twitThat(twitcontent){
   var x=Math.floor(Math.random(0)*1000)
   // var content = twitcontent+' '+x;
